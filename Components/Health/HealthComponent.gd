f02946ec4dc0329330c366659f90adac850b4db8
ellacottg <ellacottg@gmail.com>
2024-02-27 09:32:03 -0500

    breakables drop items on destruction

--- Components/Health/HealthComponent.gd
+++ Components/Health/HealthComponent.gd
@@ -4,6 +4,8 @@ class_name HealthComponent
 @export var MAX_HEALTH : int
 var health : int
 
+@export var item_drop : PackedScene
+
 func _ready():
 	health = MAX_HEALTH
 	
@@ -11,4 +13,11 @@ func damage(attack: float):
 	health -= attack
 	
 	if health <= 0:
+		if item_drop:
+			var instance = item_drop.instantiate()
+			get_parent().get_parent().add_child(instance)
+			instance.position = get_parent().position
+		
 		get_parent().queue_free()
+		
+		
